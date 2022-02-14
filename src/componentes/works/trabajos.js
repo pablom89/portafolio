800118b3260c0a2ca77ff205c075081176ff5e1f
pablom <pablo_89_cba@hotmail.com>
2022-02-14 12:02:44 -0300

    Change file trabajos.js

--- src/componentes/works/trabajos.js
+++ src/componentes/works/trabajos.js
@@ -12,7 +12,7 @@ export default [
     `Application created with React and Redux, which allows you to search for nearby parking lots based on a destination entered by the user.
     I worked with the google API, the Socket.io library and for the back-end we used both PHP and Node.js.
     By entering the address Blamey Lafore 1463, you can see garages loaded as a test.`,
-    link: "https://parking-now.netlify.app/  | https://parking-now.herokuapp.com/" ,
+    link: "https://parking-now.netlify.app/" ,
     iframe: (
       <iframe
         src="https://parking-now.netlify.app/"
